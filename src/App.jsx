9498e7d9483c775561a0c691d607d98f676a9393
Patrickodida <patrick.o.odida@gmail.com>
2024-10-04 12:53:57 +0300

    Add the Navbar component and the Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App(){
   return (
-    <>
-    </>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header className="bg-[#3DC3E4] text-[#FFFFFF] shadow-md py-4">
+            <nav className="container mx-auto flex justify-between w-[90%] m-auto m-w-[1280px] items-center">
+                <div className="text-2xl font-bold ">FortuneConnect</div>
+                <ul className="flex space-x-6">
+                    <li><Link to="/" className="hover:text-[#0F7D95] text-xl">Home</Link></li>
+                    <li><Link to="" className="hover:text-[#0F7D95] text-xl">Features</Link></li>
+                    <li><Link to="" className="hover:text-[#0F7D95] text-xl">Pricing</Link></li>
+                    <li><Link to="" className="hover:text-[#0F7D95] text-xl">Sign Up</Link></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Header;
